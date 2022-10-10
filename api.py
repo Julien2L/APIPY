eff326cda5e07bf9710b8d4efa311631cc086bed
Julien2L <julien.lehoussine-labatut@epitech.eu>
2022-10-10 17:03:31 +0200

    add raise exception

--- api.py
+++ api.py
@@ -1,7 +1,4 @@
-from lib2to3.pytree import Base
-from operator import inv
-from turtle import title
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException, status
 from typing import Optional
 from pydantic import BaseModel
 
@@ -28,19 +25,19 @@ def get_item(name: str = Query(None, title="Name", description="Name of the item
     for item_id in inventory:
         if inventory[item_id].name == name:
             return inventory[item_id]
-        return {"Data": "Not Found"}
+    raise HTTPException(status_code=404, detail="Item ID not found")
 
 @app.post("/create-item/{item_id}")
 def create_item(item_id: int, item: Item):
     if item_id in inventory:
-        return {"Error": "Item ID already exists"}
+        raise HTTPException(status_code=400, detail="Item ID already exists")
     inventory[item_id] = item
     return inventory[item_id]
 
 @app.put("/update-item/{item_id}")
 def update_item(item_id: int, item: UpdateItem):
     if item_id not in inventory:
-        return {"Error": "Item ID does not exists."}
+        raise HTTPException(status_code=404, detail="Item ID does not exists")
     if item.name != None:
         inventory[item_id].name = item.name
     if item.price != None:
@@ -52,6 +49,6 @@ def update_item(item_id: int, item: UpdateItem):
 @app.delete("/delete-item")
 def delete_item(item_id: int = Query()):
     if item_id in inventory:
-        return {"Error": "ID does not exist."}
+        raise HTTPException(status_code=404, detail="Item ID does not exist.")
     del inventory[item_id]
     return {"Succes": "Item Deleted"}
