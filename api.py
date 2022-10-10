920920e985d3450a5ca4c37140b1c655f79e9841
Julien2L <julien.lehoussine-labatut@epitech.eu>
2022-10-10 16:57:24 +0200

    initial commit

---
+++ api.py
@@ -0,0 +1,57 @@
+from lib2to3.pytree import Base
+from operator import inv
+from turtle import title
+from fastapi import FastAPI, Query
+from typing import Optional
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    brand: Optional[str] = None
+
+class UpdateItem(BaseModel):
+    name: Optional[str] = None
+    price: Optional[float] = None
+    brand: Optional[str] = None
+
+inventory = {}
+
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int):
+    return inventory[item_id]
+
+@app.get("/get-by-name")
+def get_item(name: str = Query(None, title="Name", description="Name of the item")):
+    for item_id in inventory:
+        if inventory[item_id].name == name:
+            return inventory[item_id]
+        return {"Data": "Not Found"}
+
+@app.post("/create-item/{item_id}")
+def create_item(item_id: int, item: Item):
+    if item_id in inventory:
+        return {"Error": "Item ID already exists"}
+    inventory[item_id] = item
+    return inventory[item_id]
+
+@app.put("/update-item/{item_id}")
+def update_item(item_id: int, item: UpdateItem):
+    if item_id not in inventory:
+        return {"Error": "Item ID does not exists."}
+    if item.name != None:
+        inventory[item_id].name = item.name
+    if item.price != None:
+        inventory[item_id].price = item.price
+    if item.brand != None:
+        inventory[item_id].brand = item.brand
+    return inventory[item_id]
+
+@app.delete("/delete-item")
+def delete_item(item_id: int = Query()):
+    if item_id in inventory:
+        return {"Error": "ID does not exist."}
+    del inventory[item_id]
+    return {"Succes": "Item Deleted"}
